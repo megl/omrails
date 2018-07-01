ecee966453e6900e93f50faeeb09143beb9c68f0
Mikkel Loft Mollerup <mikkelloft@gmail.com>
2018-07-01 09:20:46 +0200

    Bootstrap changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'tweets' => 'tweets'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
